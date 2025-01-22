e897e5ddccae2782bfbd9c52a1b699816eae82f5
Nolwenn Bohelay <bohelay.nolwenn@gmail.com>
2025-01-22 11:05:57 +0100

    build (mongodb): connexion à la base de donnees + routes Post et Get

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const books = require("../public/data/data.json");
+const mongoose = require("mongoose");
+const username = encodeURIComponent("Boheno");
+const password = encodeURIComponent("R@S9m95NU-Uk-sG");
+const cluster = "projet-6.ps6br.mongodb.net";
+const uri = `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority`;
+const Books = require("./models/Books");
+
+mongoose.connect(uri);
 
 const app = express();
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -12,14 +21,19 @@ app.use((req, res, next) => {
   });
 
   app.post('/api/books', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'livre ajouté !'
+    delete req.body._id;
+    const Books = new Books({
+      ...req.body
     });
-    next();
+    Books.save()
+    .then(() => res.status(201).json ({message: "Livre enregistré"}))
+    .catch(error => res.status(400).json({error}));
   });
-  app.get('/api/books', (req, res, next) => {
-    res.status(200).json(books);
+
+  app.use('/api/books', (req, res, next) => {
+    Books.find()
+    .then(Books => res.status(200).json(Books))
+    .catch(error => res.status(400).json({error}));
   });
 
 module.exports = app;
