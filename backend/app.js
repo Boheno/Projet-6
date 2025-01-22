fec14aae88d94205a8dc0d656350c9198335247f
Nolwenn Bohelay <bohelay.nolwenn@gmail.com>
2025-01-22 09:21:37 +0100

    build (server): node + nodemon + route Get et Post

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,25 @@
 const express = require('express');
+const books = require("../public/data/data.json");
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+  app.post('/api/books', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'livre ajouté !'
+    });
+    next();
+  });
+  app.get('/api/books', (req, res, next) => {
+    res.status(200).json(books);
+  });
 
 module.exports = app;
